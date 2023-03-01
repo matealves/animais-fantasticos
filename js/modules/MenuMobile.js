25209ca11d2c5b94116104608a2948db19b24512
Mateus Alves <contatomateusalves@hotmail.com>
2023-03-01 14:02:12 -0300

    refactor: menu mobile to class

--- js/modules/MenuMobile.js
+++ js/modules/MenuMobile.js
@@ -1,20 +1,36 @@
 import outsideClick from "./OutsideClick.js";
 
-export default function initMenuMobile() {
-  const menuButton = document.querySelector('[data-menu="button"]');
-  const menuList = document.querySelector('[data-menu="list"]');
-  const eventos = ["click", "touchstart"];
+export default class MenuMobile {
+  constructor(button, list, events) {
+    this.menuButton = document.querySelector(button);
+    this.menuList = document.querySelector(list);
+    this.activeClass = "active";
+    // Define argumentos padrões caso não seja passado nada
+    this.events = events === undefined ? ["click", "touchstart"] : events;
 
-  function openMenu() {
-    menuList.classList.add("active");
-    menuButton.classList.add("active");
-    outsideClick(menuList, eventos, () => {
-      menuList.classList.remove("active");
-      menuButton.classList.remove("active");
+    // Referenciar objeto no this para função callback
+    this.openMenu = this.openMenu.bind(this);
+  }
+
+  openMenu() {
+    this.menuList.classList.add(this.activeClass);
+    this.menuButton.classList.add(this.activeClass);
+    outsideClick(this.menuList, this.events, () => {
+      this.menuList.classList.remove(this.activeClass);
+      this.menuButton.classList.remove(this.activeClass);
     });
   }
 
-  if (menuButton) {
-    eventos.forEach((evento) => menuButton.addEventListener(evento, openMenu));
+  addMenuEvent() {
+    this.events.forEach((event) =>
+      this.menuButton.addEventListener(event, this.openMenu)
+    );
+  }
+
+  init() {
+    if (this.menuButton && this.menuList) {
+      this.addMenuEvent();
+    }
+    return this;
   }
 }
